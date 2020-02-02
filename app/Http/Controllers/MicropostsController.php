8bdbf0ce470e6f6d8f72695956b8cd295fc30842
s.kurahashi <work.wb14@gmail.com>
2020-02-02 10:49:59 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MicropostsController extends Controller
+{
+    // 投稿一覧表示処理
+    public function index()
+    {
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+        }
+        
+        return view('welcome', $data);
+    }
+    
+    // 投稿新規登録処理
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+
+        return back();
+    }
+    
+    // 投稿削除処理
+    public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+
+        if (\Auth::id() === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return back();
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,7 @@ use App\User;
 
 class UsersController extends Controller
 {
-    //  ユーザ一覧教示処理
+    //  ユーザ一覧表示処理
     public function index()
     {
         $users = User::orderBy('id', 'desc')->paginate(10);
@@ -22,9 +22,15 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        return view('users.show', [
+        $data = [
             'user' => $user,
-        ]);
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
